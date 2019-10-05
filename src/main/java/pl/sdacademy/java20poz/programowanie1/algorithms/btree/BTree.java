fa8977a90e9a0496ecc330d535404687e6bf26b8
Jakub Olszewski <j.b.olszewski@gmail.com>
2019-10-05 15:36:29 +0200

    Zadanie: Implementacja drzewa binarnego poszukiwań - należy zaimplementować interfejs Node w klasie NodeImpl w pakiecie btree

---
+++ src/main/java/pl/sdacademy/java20poz/programowanie1/algorithms/btree/BTree.java
@@ -0,0 +1,29 @@
+package pl.sdacademy.java20poz.programowanie1.algorithms.btree;
+
+/**
+ * Aby rozpocząć implementacje tej klasy nalezy ukończyć NodeImpl
+ */
+public class BTree {
+
+    public void insert(int i) {
+        // TODO jeśli jest pusty utwórz nowy węzeł i przypisz do korzenia
+
+        // TODO jeśli nie jest pusty to szukamy odpowiedniego rodzinca
+
+        // TODO dopuki jest zajęty węzeł, szukamy odpowiednio: jeśli nasza wartość
+        //  jest mniejsza od węzła idziemy w lewo w przeciwnym wypadku w prawo
+
+        // TODO gdy pojawi się wolny węzeł to zapiszmy sobie jego rodzica
+
+        // TODO tworzymy nowy węzeł z wartością którą chcemy ustawić
+
+        // TODO pobieramy wartość klucza tego rodzica
+
+        // TODO jeśli nasza wartość jest mniejsza to na rodzicu ustawiamy nowo stworzony węzeł jako lewy
+        // w przeciwnym wypadku ustawiamy jako prawy
+    }
+
+    public Node getRoot() {
+        return null;
+    }
+}
