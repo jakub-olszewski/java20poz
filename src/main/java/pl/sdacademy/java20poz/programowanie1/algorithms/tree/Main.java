fa2a0b20f1fbe70a2f0eba9a2ff4e1724c1a4369
Jakub Olszewski <j.b.olszewski@gmail.com>
2019-10-05 14:18:21 +0200

    Zadanie: Implementacja drzewa - rysowanie drzewa

--- src/main/java/pl/sdacademy/java20poz/programowanie1/algorithms/tree/Main.java
+++ src/main/java/pl/sdacademy/java20poz/programowanie1/algorithms/tree/Main.java
@@ -8,16 +8,24 @@ public class Main {
 
     public static void main(String[] args) {
         //List<Object> lista = new ArrayList<>();
-        Node<String> root = new NodeImpl<>(null,"Adam");
+        Node<String> root = new NodeImpl<>(null, "Adam");
 
-        Node<String> wezelEwa = new NodeImpl<>(root,"Ewa");
-        root.addChild(wezelEwa);
+        // dodajemy do niego kolejne węzły
+        Node<String> n1 = root.addChild("Ewa");
+        Node<String> n2 = root.addChild("Jarek");
+        Node<String> n3 = root.addChild("Marta");
 
-        root.addChild("Jarek");
+        n1.addChild("Jurek");
+        n1.addChild("Max");
+        n1.addChild("Iza");
 
-        //uzupelniamy ze slajdu
+        n2.addChild("Iwona");
+
+        n3.addChild("Rafał");
+        n3.addChild("Ola");
 
 
+        TreePrinter.print(root);
 
 
     }
