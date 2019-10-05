d63ed37127aea76df62cbd5519bf906c6f0358cc
Jakub Olszewski <j.b.olszewski@gmail.com>
2019-10-05 14:55:38 +0200

    Zadanie: Implementacja drzewa - stwórz metodę toString()

--- src/main/java/pl/sdacademy/java20poz/programowanie1/algorithms/tree/NodeImpl.java
+++ src/main/java/pl/sdacademy/java20poz/programowanie1/algorithms/tree/NodeImpl.java
@@ -1,56 +1,81 @@
 package pl.sdacademy.java20poz.programowanie1.algorithms.tree;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class NodeImpl<T> implements Node<T>{
 
+    private T data;
+    private List<Node<T>> children;
+    private Node<T> parent;
+
+    public NodeImpl(){
+        parent = null;
+        children = new LinkedList<>();
+    }
+
+    public NodeImpl(Node<T> parent){
+        this();
+        this.parent = parent;
+    }
+
     public NodeImpl(Node<T> parent, T data) {
-        // todo implementacja
+        this(parent);
+        this.data = data;
     }
 
     @Override
     public Node<T> getParent() {
-        return null;
+        return parent;
     }
 
     @Override
     public void setParent(Node<T> parent) {
-
+        this.parent = parent;
     }
 
     @Override
     public T getData() {
-        return null;
+        return data;
     }
 
     @Override
     public void setData(T data) {
-
+        this.data = data;
     }
 
+    /**
+     * zwracamy rozmiar listy przechowującej
+     * potomków węzła
+     * @return
+     */
     @Override
     public int getDegree() {
-        return 0;
+        return children.size();
     }
 
     @Override
     public Node<T> getChild(int i) {
-        return null;
+        return children.get(i);
     }
 
     @Override
     public boolean isLeaf() {
-        return false;
+        return children.isEmpty();
     }
 
     @Override
     public Node<T> addChild(Node<T> child) {
-        return null;
+        child.setParent(this);
+        children.add(child);
+        return child;
     }
 
     @Override
     public Node<T> addChild(T data) {
-        return null;
+        NodeImpl<T> child = new NodeImpl<T>(this, data);
+        children.add(child);
+        return child;
     }
 
     @Override
